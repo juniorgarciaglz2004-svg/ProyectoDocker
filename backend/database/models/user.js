09d8dc200652c765f57d9691e1792234d60b9153
juniorgarciaglz2004-svg <juniorgarciaglz2004@gmail.com>
2026-04-09 16:14:39 +0200

    Inicializacion del frontend

--- backend/database/models/user.js
+++ backend/database/models/user.js
@@ -3,13 +3,9 @@ import { Model } from 'sequelize';
 
 export default (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
-      // define association here
+      
     }
   }
   User.init({
