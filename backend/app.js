a8ffee519a2599ce22efcdd728acb1b1f687ae60
juniorgarciaglz2004-svg <juniorgarciaglz2004@gmail.com>
2026-04-14 21:52:52 +0200

    Cors habilitado

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 import usuarios from './rutas/usuarios.js'
 import indice from './rutas/index.js'
 import config from './servicios/config.js'
 
 const app = express()
 
-app.use(express.json());
+
+app.use(cors()) 
+app.use(express.json())
+
+
 app.use('/', indice)
 app.use('/usuarios', usuarios)
 
